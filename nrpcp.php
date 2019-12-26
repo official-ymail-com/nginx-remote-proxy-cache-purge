fa7097a72fb93fc6a6706e845118fe0fc61440e2
official-ymail-com <46469710+official-ymail-com@users.noreply.github.com>
2019-12-27 01:51:05 +0800

    remove define

--- nrpcp.php
+++ nrpcp.php
@@ -10,12 +10,5 @@ define( 'VERSION_nrpcp_PLUGIN',  '1.0');
 define( 'PATH_nrpcp_PLUGIN',  plugin_dir_path( __FILE__ ));
 define( 'URL_nrpcp_PLUGIN',  plugins_url('', __FILE__));
 
-if(!defined('nrpcp_secret')){
-	define( 'nrpcp_secret',  'Nginx Remote Proxy Cache Purge Secrete');
-}
-if(!defined('nrpcp_purge_folder')){
-	define( 'nrpcp_purge_folder',  'purge');
-}
-
 require_once(PATH_nrpcp_PLUGIN . 'class/nrpcp.class.php');
 add_action( 'plugins_loaded', array( 'nrpcp_class', 'init' ) );
